488f9bbb78e4bcc847154e639a5820e27f263712
gdow1409 <72255189+gdow1409@users.noreply.github.com>
2021-12-28 22:15:33 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ playerimg2 = loadImage("left.png")
 Coinimg = loadImage("coin.png")
 Hammerimg = loadImage("hammer.png")
 Heartimg = loadImage("Heart.png")
-Backgroundimg = loadImage("background.jpg")
+Backgroundimg = loadImage("backdrop.jpg")
 Stoneimg = loadImage("Stone wall.png")
 }
 
